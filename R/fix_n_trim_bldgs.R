aaa8a38f2da941594145cacf38295d3c2d025f80
M. Marek-Spartz <patt0335@umn.edu>
2026-04-20 10:51:20 -0500

    Update fix_n_trim_bldgs.R with direct imports

--- R/fix_n_trim_bldgs.R
+++ R/fix_n_trim_bldgs.R
@@ -17,19 +17,19 @@
 fix_n_trim_bldgs<-function(blds, rad){
   
   cty_ex<-terra::ext(rad)
-  bbox <- st_bbox(cty_ex, crs = 4326) 
-  bbox_sf <- st_as_sfc(bbox)
-  sf_use_s2(FALSE) #turn off spherical geometry
-  blds<-st_filter(blds, bbox_sf)
+  bbox <- sf::st_bbox(cty_ex, crs = 4326) 
+  bbox_sf <- sf::st_as_sfc(bbox)
+  sf::sf_use_s2(FALSE) #turn off spherical geometry
+  blds<-sf::st_filter(blds, bbox_sf)
   gc()
   
-  invalid_geometries <- blds[!st_is_valid(blds), ] 
+  invalid_geometries <- blds[!sf::st_is_valid(blds), ] 
   if(nrow(invalid_geometries)>0){
     x_blds<-as.numeric(rownames(invalid_geometries))
     blds<-blds[-c(x_blds),]
-    sf_use_s2(TRUE)
+    sf::sf_use_s2(TRUE)
   }else{
-    sf_use_s2(TRUE)
+    sf::sf_use_s2(TRUE)
     return(blds)
   }
 }
