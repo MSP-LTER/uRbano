d25b0c2930ef61258980c872dcfeb42030453c43
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2026-08-19 11:49:07 -0500

    Use the standard way of using magrittr pipes inside a package without re-exporting it

---
+++ R/utils-pipe.R
@@ -0,0 +1,11 @@
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
